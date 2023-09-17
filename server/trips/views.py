f86faf572defdb174d3afd69a2b528c9270fd956
Nicolas Granados <nicogranados1210@gmail.com>
2023-09-17 21:11:59 +0200

    first commit

---
+++ server/trips/views.py
@@ -0,0 +1,22 @@
+
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions, viewsets # changed
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .models import Trip # new
+from .serializers import LogInSerializer, TripSerializer, UserSerializer # changed
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class LogInView(TokenObtainPairView): # new
+    serializer_class = LogInSerializer
+
+class TripView(viewsets.ReadOnlyModelViewSet):
+    lookup_field = 'id' # new
+    lookup_url_kwarg = 'trip_id' # new
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
