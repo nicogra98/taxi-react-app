7481f8826e28ecad9b79a43cba42c2993e21e46f
Nicolas Granados <nicogranados1210@gmail.com>
2023-10-30 23:01:26 +0100

    frontend

---
+++ client/src/components/LogIn.js
@@ -0,0 +1,26 @@
+// client/src/components/LogIn.js
+
+import React from 'react';
+import { Breadcrumb, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function LogIn (props) {
+  return (
+    <>
+      <Breadcrumb>
+        <Breadcrumb.Item href='/#/'>Home</Breadcrumb.Item>
+        <Breadcrumb.Item active>Log in</Breadcrumb.Item>
+      </Breadcrumb>
+      <Card>
+        <Card.Header>Log in</Card.Header>
+        <Card.Body>
+          <Card.Text className='text-center'>
+            Don't have an account? <Link to='/sign-up'>Sign up!</Link>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
+
+export default LogIn;
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,26 @@
+// client/components/SignUp.js
+
+import React from 'react';
+import { Breadcrumb, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function SignUp (props) {
+    return (
+      <>
+        <Breadcrumb>
+          <Breadcrumb.Item href='/#/'>Home</Breadcrumb.Item>
+          <Breadcrumb.Item active>Sign up</Breadcrumb.Item>
+        </Breadcrumb>
+        <Card>
+          <Card.Header>Sign up</Card.Header>
+          <Card.Body>
+            <Card.Text className='text-center'>
+              Already have an account? <Link to='/log-in'>Log in!</Link>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </>
+    );
+  }
+  
+  export default SignUp;
